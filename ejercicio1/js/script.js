aec77de814c8baefca0517f0066bd05e1938b00e
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-11-28 13:42:17 +0100

    commit

--- ejercicio1/js/script.js
+++ ejercicio1/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-    // Obtener y mostrar la cantidad de capas creadas usando el método size()
-    const numeroDeCapas = $('.capa').size(); // size() en jQuery 1.x
+    const numeroDeCapas = $('.capa').length;
     alert(`Número de capas creadas: ${numeroDeCapas}`);
 
     // Cambiar el color de texto de todas las capas a verde
